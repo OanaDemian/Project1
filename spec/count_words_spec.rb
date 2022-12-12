93d6ba59cee88ade0b1189e716793f3d8db8f481
mantaslv <mantasvolkas@gmail.com>
2022-12-12 15:33:53 +0000

    count_words method, ready to add test for more spaces

---
+++ spec/count_words_spec.rb
@@ -0,0 +1,20 @@
+require 'count_words'
+
+RSpec.describe "count_words method" do
+    it "takes in text and returns correct no. of words." do
+        result = count_words("It is a snowy day.")
+        expect(result).to eq 5
+    end
+
+    context "when string is empty" do
+        it "fails" do
+            expect{ count_words("") }.to raise_error "Please enter some text!"
+        end
+    end
+
+    context "when given something other than string" do
+        it "fails" do
+            expect{ count_words(12345) }.to raise_error "Incorrect input type: please enter text!"
+        end
+    end
+end
