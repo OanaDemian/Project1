fa898507efd5e913ed3218c22c25b7be4f9ade62
mantaslv <mantasvolkas@gmail.com>
2022-12-14 16:42:10 +0000

    DiaryEntry completed

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -1,7 +1,13 @@
 class DiaryEntry
   def initialize(title, contents) # title, contents are strings
+    fail "Error: Title is not text!" unless title.is_a? String
+    fail "Error: Contents are not text!" unless contents.is_a? String
+    raise "Error: No title given!"    if title    == "" || title.match?(/^\s+$/)
+    raise "Error: No contents given!" if contents == "" || contents.match?(/^\s+$/)
     @title = title
     @contents = contents
+    @words = contents.split(" ")
+    @current_word = 0
   end
 
   def title
@@ -16,12 +22,14 @@ class DiaryEntry
 
   def count_words
     # Returns the number of words in the contents as an integer
+    @words.count
   end
 
   def reading_time(wpm) # wpm is an integer representing the number of words the
                         # user can read per minute
     # Returns an integer representing an estimate of the reading time in minutes
     # for the contents at the given wpm.
+    (count_words.to_f / wpm).ceil
   end
 
   def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
@@ -33,5 +41,11 @@ class DiaryEntry
     # If called again, `reading_chunk` should return the next chunk, skipping
     # what has already been read, until the contents is fully read.
     # The next call after that it should restart from the beginning.
+    words_to_read = wpm * minutes
+    @current_word
+    chunk = @words[@current_word...@current_word + words_to_read].join(" ")
+    @current_word += words_to_read
+    @current_word = 0 if @current_word >= count_words
+    return chunk
   end
 end
--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -1,13 +1,73 @@
 require 'diary_entry'
 
 RSpec.describe "diary_entry method" do
-  it "returns the title when given a title and its contents" do
+  context "when given a title and its contents" do
     diary = DiaryEntry.new("GP Practice", "020 212 333")
-    expect(diary.title).to eq "GP Practice"
+  
+    it "returns the title" do
+      expect(diary.title).to eq "GP Practice"
+    end
+
+    it "returns the contents" do
+      expect(diary.contents).to eq "020 212 333"
+    end
+
+    it "returns no. of words in contents" do
+      expect(diary.count_words).to eq 3
+    end
+
+    it "returns estimate of reading contents in minutes when wpm is given" do
+      expect(diary.reading_time(200)).to eq 1
+    end
+
+    it "returns estimate of reading contents in minutes when wpm is given" do
+      expect(diary.reading_time(1)).to eq 3
+    end
   end
 
-  it "returns the contents when given a title and its contents" do
-    diary = DiaryEntry.new("GP Practice", "020 212 333")
-    expect(diary.contents).to eq "020 212 333"
+  context "when given wpm and no. of mins user has to read" do
+    diary = DiaryEntry.new("Letter to Santa", "Dear Santa, for Christmas I would like to receive a new jumper.")
+
+    it "returns contents that user can read in that time" do
+      expect(diary.reading_chunk(2, 2)).to eq "Dear Santa, for Christmas"
+    end
+
+    it "skips content already read and returns next contents to be read" do
+      expect(diary.reading_chunk(2, 3)).to eq "I would like to receive a"
+    end
+
+    it "returns last words from content" do
+      expect(diary.reading_chunk(2, 4)).to eq "new jumper."
+    end
+
+    it "starts from beginning after all content read" do
+      expect(diary.reading_chunk(3, 3)).to eq "Dear Santa, for Christmas I would like to receive"
+    end
+  end
+
+  context "fails if" do
+    it "title is not a string" do
+      expect{ DiaryEntry.new(1234, "adsfadsf") }.to raise_error "Error: Title is not text!"
+    end
+
+    it "contents are not a string" do
+      expect{ DiaryEntry.new("adsfadsf", 1234) }.to raise_error "Error: Contents are not text!"
+    end
+
+    it "title is empty" do
+      expect{ DiaryEntry.new("", "asda") }.to raise_error "Error: No title given!"
+    end
+
+    it "contents are empty" do
+      expect{ DiaryEntry.new("asda", "") }.to raise_error "Error: No contents given!"
+    end
+
+    it "title is empty" do
+      expect{ DiaryEntry.new("    ", "asda") }.to raise_error "Error: No title given!"
+    end
+
+    it "contents are empty" do
+      expect{ DiaryEntry.new("asda", "    ") }.to raise_error "Error: No contents given!"
+    end
   end
 end
